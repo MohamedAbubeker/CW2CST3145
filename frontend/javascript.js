100f4328ce996890fb940ff8ae723b4572c580ce
mrko147 <95499236+mrko147@users.noreply.github.com>
2023-02-22 16:09:04 +0000

    undo the fetch function

--- frontend/javascript.js
+++ frontend/javascript.js
@@ -69,7 +69,7 @@ const app = Vue.createApp({
         },
 
         async checkoutOrder(name, phoneNumber){
-            //this.cart.push({"name": this.name, "phoneNumber": this.phoneNumber});
+            this.cart.push({"name": this.name, "phoneNumber": this.phoneNumber});
             localStorage.setItem("cart", JSON.stringify(this.cart));
             let tempCart = localStorage.getItem("cart");
            
@@ -77,13 +77,6 @@ const app = Vue.createApp({
             fetch('http://localhost:4000/lessons', {
                 method: "POST",
                 body: tempCart,
-                headers: {"Content-type": "application/json; charset=UTF-8"}
-            }).then((res) => console.log("lesson added"))
-             .catch((err) => console.log(err))
-
-             fetch('http://localhost:4000/lessons/cart', {
-                method: "POST",
-                body: {"name": this.name, "phoneNumber": this.phoneNumber},
                 headers: {"Content-type": "application/json; charset=UTF-8"}
             }).then((res) => console.log("lesson added"))
              .catch((err) => console.log(err))
